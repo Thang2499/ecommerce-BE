d61238bae47e26096d48b074dfbe619e73b38329
jaki5646 <vinhlocson@gmail.com>
2025-01-20 20:20:14 +0700

    thêm các comment

--- src/middlewares/adminMiddleware/adminMiddleware.js
+++ src/middlewares/adminMiddleware/adminMiddleware.js
@@ -41,18 +41,21 @@ const adminMiddleware = {
             const token = req.headers.authorization.split(' ')[1];
             const { email, password, name, phone, address } = req.body;
 
+            // check token có valid hay không; valid thì check real time role có phải là SUPER_ADMIN hay không
             const decodedToken = tokenService.verifyToken(token);
-
             const admin = await adminModel.findOne({ email: decodedToken.admin.email });
 
             if (admin.role !== 'SUPER_ADMIN' || !admin.isActived) {
                 return res.send('Ban khong co quyen');
             }
 
+            // các trường required
             if (!email || !password || !name || !phone || !address) {
                 return res.send('Vui long dien day du thong tin');
             }
 
+
+            // self explanatory
             const duplicate = await adminModel.findOne({
                 $or: [
                     { email },
