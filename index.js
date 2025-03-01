aee2fd9c70596f24e24b1fb9820c03f679473338
thang2499 <buiducthang0204@gmail.com>
2025-03-01 20:49:28 +0700

    rebuild

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ app.use(cookieParser());
 //     origin: 'http://localhost:5173', 
 //     credentials: true,               
 // }));
-// app.use(cors({
-//     origin: "https://ecommerce-fe-blond.vercel.app",
-//     credentials: true,
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   }));
-  app.use(cors());
+app.use(cors({
+    origin: "https://ecommerce-fe-blond.vercel.app",
+    credentials: true,
+    methods: "*",
+  }));
 app.use(bodyParser.json());
 app.use('',indexRoute);
 
